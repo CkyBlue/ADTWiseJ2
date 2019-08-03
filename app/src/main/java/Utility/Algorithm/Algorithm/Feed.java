5f919027d47fce6ed60073cebb3560e086aa1518
CkyBlue <sakrit.karmacharya77@gmail.com>
2019-08-03 15:37:12 +0545

    Completed refactoring Algorithm.Commands

--- app/src/main/java/Utility/Algorithm/Algorithm/Feed.java
+++
@@ -1,6 +0,0 @@
-package Utility.Algorithm.Algorithm;
-
-import Utility.Bases.SuperFeed;
-
-public class Feed extends SuperFeed<Content, Printer> {
-}
--- app/src/main/java/Utility/Algorithm/Algorithm/Printer.java
+++
@@ -1,6 +0,0 @@
-package Utility.Algorithm.Algorithm;
-
-import Utility.Bases.SuperPrinter;
-
-public abstract class Printer extends SuperPrinter<Content, Feed> {
-}
